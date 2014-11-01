6afb378e498c664b2731b67e08d5bd5ce0a2801b
maharsh007 <maharshi.choudhury@gmail.com>
2014-11-01 13:58:38 +0530

    The files
    Game source

---
+++ game.js
@@ -0,0 +1,6 @@
+var game=new Phaser.Game(w,h,Phaser.AUTO,'gameDiv');
+game.state.add('Load',Game.Load);
+game.state.add('Play',Game.Play);
+game.state.add('End',Game.End);
+
+game.state.start('Load');
